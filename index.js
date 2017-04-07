e69f54822194acc39cd7e852afe073fc54f63c4c
Noah Prescott <noah.prescott@gmail.com>
2017-04-07 16:36:37 -0400

    Add request header parser application

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var morgan = require('morgan');
+var app = express();
+
+// Views config
+app.set("views", "./views");
+app.set("view engine", "pug");
+app.use(express.static(__dirname + '/public'));
+
+// Middleware
+app.use(morgan('combined'));
+
+// Routes
+app.get("/", function (req, res) {
+  var ipReq = req.ip;
+  var languageReq = req.get("Accept-Language");
+  var softwareReq = req.get("User-Agent");
+  res.render("index", {ip: JSON.stringify(ipReq),
+                       language: JSON.stringify(languageReq),
+                       software: JSON.stringify(softwareReq)}, function(err, html) {
+                         res.send(html);
+                         res.end();
+                       });
+});
+
+// Listen on port
+app.listen(process.env.PORT || 5000);
