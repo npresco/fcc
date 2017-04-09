621d3616ca1b675feac25d6d74b5ee143452d832
Noah Prescott <noah.prescott@gmail.com>
2017-04-09 16:06:59 -0400

    Add valid url package and check for url generation

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const express = require("express");
 const morgan = require("morgan");
 const MongoClient = require("mongodb").MongoClient;
 const path = require("path");
+const validUrl = require("valid-url");
 
 const app = express();
 
@@ -26,34 +27,41 @@ MongoClient.connect(process.env.MONGODB_URI, (err, database) => {
   // Routes
   app.get("/", (req, res) => {
     // Display project use cases
-    res.end("Hello");
+    const hostUrl = `${req.protocol}://${req.get("host")}`;
+    res.render("index.pug", { url: hostUrl });
   });
 
   app.get("/new/:url(*)", (req, res) => {
     // create shortend url
     const queryUrl = req.params.url;
-    const hostUrl = `${req.protocol}://${req.get("host")}`;
+    if (validUrl.isUri(queryUrl)) {
+      const hostUrl = `${req.protocol}://${req.get("host")}`;
 
-    dbUrls.count().then((numIds) => {
-      dbUrls.findOne({ originalUrl: queryUrl }, (err2, result) => {
-        if (err2) return console.warn(err2);
-        if (result) {
-          res.end(JSON.stringify(result));
-        } else {
-          dbUrls.insertOne({
-            _id: numIds + 1,
-            shortenedUrl: `${hostUrl}/${numIds + 1}`,
-            originalUrl: queryUrl,
-          }, (err3, doc) => {
-            if (err3) return console.warn(err3);
-            res.end(JSON.stringify(doc.ops));
-            return undefined;
-          },
-                          );
-        }
-        return undefined;
+      dbUrls.count().then((numIds) => {
+        dbUrls.findOne({ originalUrl: queryUrl }, (err2, result) => {
+          if (err2) return console.warn(err2);
+          if (result) {
+            res.end(JSON.stringify(result));
+          } else {
+            dbUrls.insertOne(
+              {
+                _id: numIds + 1,
+                shortenedUrl: `${hostUrl}/${numIds + 1}`,
+                originalUrl: queryUrl,
+              },
+              (err3, doc) => {
+                if (err3) return console.warn(err3);
+                res.end(JSON.stringify(doc.ops));
+                return undefined;
+              },
+            );
+          }
+          return undefined;
+        });
       });
-    });
+    } else {
+      res.end("Not a valid URL, try again!");
+    }
   });
 
   app.get("/:id", (req, res) => {
